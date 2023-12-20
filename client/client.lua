ef076743e9ab70625c31af0cfcb6002daf3b20e0
Apollomcflurry <theambushdnb@gmail.com>
2023-12-20 16:19:22 -0500

    rsg-canoe qol improvements
    added a water check so the player has to be standing in the water to launch the canoe.  As of right now player can use canoe 20 feet away from river and float  in the air ,   This fix gets rid of that.
    Switched notifys over to ox
    added new notify for if you are not standing in the water.

--- client/client.lua
+++ client/client.lua
@@ -8,36 +8,48 @@ AddEventHandler('rsg-canoe:client:lauchcanoe', function()
         if boatout == false then
             local ped = PlayerPedId()
             local coords = GetEntityCoords(ped)
-            local water = Citizen.InvokeNative(0x5BA7A68A346A5A91, coords.x+3, coords.y+3, coords.z)
-            local canLauch = false
-            for k,v in pairs(Config.WaterTypes) do 
-                if water == Config.WaterTypes[k]["waterhash"]  then
-                    canLauch = true           
-                    break            
+            
+            -- Increase the range to check for water
+            local water = Citizen.InvokeNative(0x5BA7A68A346A5A91, coords.x, coords.y, coords.z)
+            
+            -- Check if the player is in water
+            if water then
+                local canLaunch = false
+                local playerPos = GetEntityCoords(ped)
+                
+                -- Iterate through water areas to find if the player is in one
+                for k, v in pairs(Config.WaterTypes) do
+                    local waterType = Config.WaterTypes[k].waterhash
+                    if water == waterType and IsEntityInWater(ped) then
+                        canLaunch = true
+                        break
+                    end
                 end
-            end
-            if canLauch then
-                local ped = PlayerPedId()
-                local canoe = `CANOE`
-                local x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(ped, 0.0, 4.0, 0.5 ))
-                local heading = GetEntityHeading(ped)
-                RequestModel(canoe)
-                while not HasModelLoaded(canoe) do
-                    Citizen.Wait(500)
+                
+                if canLaunch then
+                    local canoeModel = `CANOE`
+                    local x, y, z = table.unpack(GetOffsetFromEntityInWorldCoords(ped, 0.0, 4.0, 0.5))
+                    local heading = GetEntityHeading(ped)
+                    RequestModel(canoeModel)
+                    while not HasModelLoaded(canoeModel) do
+                        Citizen.Wait(500)
+                    end
+                    activeboat = CreateVehicle(canoeModel, x, y, z, heading, 1, 1)
+                    SetVehicleOnGroundProperly(activeboat)
+                    SetPedIntoVehicle(ped, activeboat, -1)
+                    SetModelAsNoLongerNeeded(canoeModel)
+                    boatout = true
+                else
+                    lib.notify({ title = Lang:t('error.not_in_correct_water'), duration = 5000, type = 'error' })
                 end
-                activeboat = CreateVehicle(canoe, x, y, z, heading, 1, 1)
-                SetVehicleOnGroundProperly(activeboat)
-                SetPedIntoVehicle(ped, activeboat, -1)
-                SetModelAsNoLongerNeeded(canoe)
-                boatout = true
             else
-                RSGCore.Functions.Notify(Lang:t('error.cant_take_out_boat'), 'error')
+                lib.notify({ title = Lang:t('error.cant_take_out_boat'), duration = 5000, type = 'error' })
             end
         else
-            RSGCore.Functions.Notify(Lang:t('error.boat_already_out'), 'error')
+            lib.notify({ title = Lang:t('error.boat_already_out'), duration = 5000, type = 'error' })
         end
     else
-        RSGCore.Functions.Notify(Lang:t('error.no_item'), 'error')
+        lib.notify({ title = Lang:t('error.no_item'), duration = 5000, type = 'error' })
     end
 end)
 
--- locales/en.lua
+++ locales/en.lua
@@ -3,6 +3,7 @@ local Translations = {
         cant_take_out_boat = 'You can\'t take out your canoe here!',
         boat_already_out = 'You already have your canoe out!',
         no_item = 'You don\'t have this item!',
+        not_in_correct_water = 'You are not standing in the water',
     },
 }
 
@@ -11,4 +12,3 @@ Lang = Locale:new({
     warnOnMissing = true
 })
 
--- Lang:t('error.horse_too_far')
